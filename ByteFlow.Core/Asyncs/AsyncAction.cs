61c186fd9c7b000e6ea0836068bbff33213dc08a
Admin <Admin@qq.com>
2021-03-09 13:09:28 +0800

    v1.1.0

--- ByteFlow.Core/Asyncs/AsyncAction.cs
+++ ByteFlow.Core/Asyncs/AsyncAction.cs
@@ -1,9 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ByteFlow.Asyncs
 {
-    public delegate Task AsyncAction();
-    public delegate Task AsyncAction<in T>(T t);
-    public delegate Task AsyncAction<in T1, in T2>(T1 t1, T2 t2);
-    public delegate Task AsyncAction<in T1, in T2, in T3>(T1 t1, T2 t2, T3 t3);
+    public delegate Task AsyncAction(CancellationToken cancellationToken = default);
+    public delegate Task AsyncAction<in T>(T t, CancellationToken cancellationToken = default);
+    public delegate Task AsyncAction<in T1, in T2>(T1 t1, T2 t2, CancellationToken cancellationToken = default);
+    public delegate Task AsyncAction<in T1, in T2, in T3>(T1 t1, T2 t2, T3 t3, CancellationToken cancellationToken = default);
 }
--- ByteFlow.Core/Asyncs/AsyncFunc.cs
+++ ByteFlow.Core/Asyncs/AsyncFunc.cs
@@ -1,9 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ByteFlow.Asyncs
 {
-    public delegate Task<TResult> AsyncFunc<TResult>();
-    public delegate Task<TResult> AsyncFunc<in T, TResult>(T t);
-    public delegate Task<TResult> AsyncFunc<in T1, in T2, TResult>(T1 t1, T2 t2);
-    public delegate Task<TResult> AsyncFunc<in T1, in T2, in T3, TResult>(T1 t1, T2 t2, T3 t3);
+    public delegate Task<TResult> AsyncFunc<TResult>(CancellationToken cancellationToken = default);
+    public delegate Task<TResult> AsyncFunc<in T, TResult>(T t, CancellationToken cancellationToken = default);
+    public delegate Task<TResult> AsyncFunc<in T1, in T2, TResult>(T1 t1, T2 t2, CancellationToken cancellationToken = default);
+    public delegate Task<TResult> AsyncFunc<in T1, in T2, in T3, TResult>(T1 t1, T2 t2, T3 t3, CancellationToken cancellationToken = default);
 }
